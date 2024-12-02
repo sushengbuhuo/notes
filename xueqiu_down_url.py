372f0cface0111166c178671080ed8c76855f2ef
susheng <susheng@gmail.com>
2024-12-02 18:28:56 +0800

    up

--- xueqiu_down_url.py
+++ xueqiu_down_url.py
@@ -44,7 +44,8 @@ def down(url):
         res = requests.get(url, headers=headers)
         try:
             timestamp = re.search(r'data-created_at="(.*?)"', res.text).group(1)
-            date = time.strftime('%Y-%m-%d', time.localtime(int(timestamp) / 1000))
+            date = time.strftime('%Y-%m-%d %H%M%S', time.localtime(int(timestamp) / 1000))
+            date2 = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(timestamp) / 1000))
             soup = BeautifulSoup(res.text, 'html.parser')
             titlesoup = soup.find("h1", {"class": "article__bd__title"})
             article_json = re.findall(r'<script>window\.SNOWMAN_STATUS = (.*?);\n',res.text,flags=re.S)
@@ -52,7 +53,7 @@ def down(url):
             title = data['title']
             # if titlesoup:
             #     title = titlesoup.get_text()
-            article_html = re.search(r'<article class="article__bd">(.*)</article>', res.text).group(1)
+            article_html = re.search(r'<article class="article__bd">(.*)</article>', res.text).group(1) #data['text']
             if not title:
                 # title = soup.find("article", {"class": "article__bd"}).get_text() 
                 title = re.sub(r'<.*?>', '', data['description'])
@@ -67,14 +68,27 @@ def down(url):
                 reply_count =  data['retweeted_status']['reply_count']
                 like_count =  data['retweeted_status']['like_count']
                 retweeted = f'<br><div class="timeline__item__forward__hd"><a href="https://xueqiu.com/{user_id}" target="_blank" data-tooltip="{user_id}" analytics-data="&quot;&quot;" class="user-name-link"><span class="user-name">@{user_name}</span><span><h-char unicode="ff1a" class="biaodian cjk bd-end bd-jiya"><h-inner>：</h-inner></h-char></span></a></div><div class="timeline__item__forward__content">'+data['retweeted_status']['text']+f'</div><br><div class="timeline__item__forward__ft"><span class="timestamp">{timeBefore}</span><span class="retweet-count"> <h-char unicode="b7" class="biaodian cjk bd-middle bd-jiya"><h-inner>·</h-inner></h-char> 转发 {retweet_count}</span><a href="/8852934528/312437395#comment" target="_blank" class="replay-count"> <h-char unicode="b7" class="biaodian cjk bd-middle bd-jiya"><h-inner>·</h-inner></h-char> 讨论 {reply_count}</a><span class="like-count"> <h-char unicode="b7" class="biaodian cjk bd-middle bd-jiya"><h-inner>·</h-inner></h-char> 赞 {like_count}</span><!----></div>'
+                image = ''
+                if data['retweeted_status']['pic']:
+                    pics = data['retweeted_status']['pic'].split(',')
+                    for pic in pics:
+                        imageUrl = pic.replace('thumb.jpg','800.jpg')
+                        image += f'<br><img data-src="{imageUrl}" src="{imageUrl}" lazy="loaded">'
+                retweeted += image
             if not title:
                 title = re.search(r'https://xueqiu.com/\d+/(\d+)', url).group(1)
-            article_html = '<h4 class="">发布时间：'+date+'</h4>'+article_html+retweeted
+            image = ''
+            if data['pic'] and not data['title']:
+                pics = data['pic'].split(',')
+                for pic in pics:
+                    imageUrl = pic.replace('thumb.jpg','800.jpg')
+                    image += f'<br><img data-src="{imageUrl}" src="{imageUrl}" lazy="loaded">'
+            article_html = '<h4 class="">发布时间：'+date2+'</h4>'+article_html+retweeted+image
             article_content = f'<!DOCTYPE html><html><head><meta charset="utf-8"></head><body><div class="article-content">{article_html}</article></div></body></html>'
             # article_content=res.text
             print('开始下载',url,title)
             save_history(url)
-            with open(f'html/'+date+'_'+trimName(title)+'.html', 'w', encoding='utf-8') as f:
+            with open(f'xueqiu/'+date+'_'+trimName(title)+'.html', 'w', encoding='utf-8') as f:
                 f.write(article_content.replace('<p style="display:none;">','<p style="">'))
         except Exception as err:
             print('出错了',err,url);raise Exception("抓取失败了："+url)
@@ -117,12 +131,15 @@ if file_extension == '.xlsx':
         repost=str(row['转发数'])
         fav = str(row['点赞数'])
         comment = str(row['评论数'])
+        itemtime = str(row['雪球时间'])
         if comment == 'nan':
             comment = '0'
         if fav == 'nan':
             fav='0'
+        if itemtime == 'nan':
+            itemtime='0'
         with open(f'{filename}.csv', 'a+', encoding='utf-8-sig') as f:
-            f.write(row['雪球时间']+','+'https://xueqiu.com'+row['雪球链接'] + ','+repost+ ','+fav+ ','+comment+'\n')
+            f.write(itemtime+','+'https://xueqiu.com'+row['雪球链接'] + ','+repost+ ','+fav+ ','+comment+'\n')
     # for i in tqdm(df['雪球链接'].tolist(), desc='下载进度'):
     #     t=down('https:'+i)
         # break
